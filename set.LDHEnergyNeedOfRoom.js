04691891de2e35d65349d1ecf2fb9d079a6b51ef
Thomas Cibils <thomas.cibils@gmail.com>
2018-03-31 11:53:26 +0200

    Converting computed info in room need
    Adding the needs for :
     - LDFHarvesters
     - LDFMovers
    with the right data.
    Scheduling a need for a reserver creep as well.

--- set.LDHEnergyNeedOfRoom.js
+++ set.LDHEnergyNeedOfRoom.js
@@ -11,7 +11,57 @@ var setLDHEnergyNeedOfRoom = {
 				for(let sourceIndex = 0; sourceIndex < Memory.rooms[roomInMemory].sourcesHomeRooms; sourceIndex++) {
 					// If the home room is the room we're treating 
 					if(Memory.rooms[roomInMemory].sourcesHomeRooms[sourceIndex] == treatedRoom) {
-					
+						
+						// We count the existing LDHarvesters for target room AND target source
+						 let LDFHarvestersOfSource = _.filter(Game.creeps, (creep) =>
+							creep.memory.role == 'longDistanceFatHarvester' &&
+							creep.memory.targetRoom == roomInMemory &&
+							creep.memory.needOrigin == Memory.rooms[roomInMemory].sources[sourceIndex] &&
+							(creep.ticksToLive > 150 || creep.memory.creepSpawning)
+						).length;
+						
+						// And create the LDFHarvester need for the treated room
+						treatedRoom.memory.labels.push('LDFHarvester')
+						treatedRoom.memory.need.push(Memory.rooms[roomInMemory].sourcesWorkNeed[sourceIndex]);
+						treatedRoom.memory.attached.push(LDFHarvestersOfSource);
+						treatedRoom.memory.role.push('longDistanceFatHarvester');
+						treatedRoom.memory.unity.push('Number of creeps');
+						treatedRoom.memory.targetRoom.push(roomInMemory);
+						treatedRoom.memory.needOrigin.push(Memory.rooms[roomInMemory].sources[sourceIndex]);
+						treatedRoom.memory.criticalNeed.push(false);
+
+						
+						// We count the existing LDMovers for target room AND target source
+						let LDFMoversOfSource = _.filter(Game.creeps, (creep) =>
+							creep.memory.role == 'longDistanceFastMover' &&
+							creep.memory.targetRoom == roomInMemory &&
+							creep.memory.needOrigin == Memory.rooms[roomInMemory].sources[sourceIndex] &&
+							(creep.ticksToLive > 150 || creep.memory.creepSpawning)
+						).length;
+						
+						// And create the LDFMover need for the treated room
+						treatedRoom.memory.labels.push('LDFMover')
+						treatedRoom.memory.need.push(Memory.rooms[roomInMemory].sourcesCarryNeed[sourceIndex]);
+						treatedRoom.memory.attached.push(LDFMoversOfSource);
+						treatedRoom.memory.role.push('longDistanceFastMover');
+						treatedRoom.memory.unity.push('Number of creeps');
+						treatedRoom.memory.targetRoom.push(roomInMemory);
+						treatedRoom.memory.needOrigin.push(Memory.rooms[roomInMemory].sources[sourceIndex]);
+						treatedRoom.memory.criticalNeed.push(false);
+						
+						
+						/*  // Reserver creep for the room as well ?
+						treatedRoom.memory.labels.push('LDReserver')
+						treatedRoom.memory.need.push();
+						treatedRoom.memory.attached.push();
+						treatedRoom.memory.role.push();
+						treatedRoom.memory.unity.push();
+						treatedRoom.memory.targetRoom.push();
+						treatedRoom.memory.needOrigin.push();
+						treatedRoom.memory.criticalNeed.push();
+						*/
+						
+						
 					}
 				}
 			}	
