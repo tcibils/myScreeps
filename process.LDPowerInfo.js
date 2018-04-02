e6b1ac69330cbef9406fdb24c0be7350668ba684
Thomas Cibils <thomas.cibils@gmail.com>
2018-04-02 11:01:51 +0200

    PowerSource Computing - first sturcture
     - Parsing all rooms and power structures
     - Assessing lifetime of power source
     - Assessing space around power source

--- process.LDPowerInfo.js
+++ process.LDPowerInfo.js
@@ -2,11 +2,37 @@
 var processLDPowerInfo = {
     run: function() {
         
+		// Parameters :
+		let minimumPowerSourceLivingTime = 3500;
+		
+		
 		// Starting now :
 		// For each of rooms having a memory
 		for(var roomInMemory in Memory.rooms) {
+			// If there are some power sources
 			if(Memory.rooms[roomInMemory].powerSources.length > 0) {
-				
+				// For each power source
+				for(let powerSourceIndex = 0; powerSourceIndex < Memory.rooms[roomInMemory].powerSources.length; powerSourceIndex++) {
+					// We need to assess if we will harvest it.
+					
+					// Condition 1 : will it live long enough for us to have the time to harvest it ?
+					let powerSourceLivingLongEngough = false;
+					if(Memory.rooms[roomInMemory].powerSourcesTime[powerSourceIndex] > minimumPowerSourceLivingTime) {
+						powerSourceLivingLongEngough = true;
+					}
+					
+					// Condition 2 : do we have enough space around it to harvest it without boosts ?
+					let powerSourceEnoughSpace = false;
+					if(Memory.rooms[roomInMemory].powerSourceFreeSpots[powerSourceIndex] > 1) {
+						powerSourceEnoughSpace = true;
+					}
+					
+					// If we have the above conditions
+					if(powerSourceLivingLongEngough && powerSourceEnoughSpace) {
+						// We assess the distances - not before, as this is way more costly to do.
+						
+					}
+				}
         // Condition 1 sur le nombre de ticks - je dirais 3500 mini restants
 		// Condition 2 sur la proximité - Il nous faut les deux rooms non-encore power-occupées les plus proches à moins de 200 cases
 		
