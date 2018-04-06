fddceccb97f187a16f8dc4177cbbd054b2cb8630
Thomas Cibils <thomas.cibils@gmail.com>
2018-04-06 23:12:40 +0200

    typo

--- main.js
+++ main.js
@@ -198,8 +198,8 @@ module.exports.loop = function () {
 
         myRooms[currentRoomIndex].memory.labels.push('Power Spreader');
 		
-		if(Game.getObjectById(myRooms[currentRoomIndex].memory.storageOfRoom[0]) != undefined) {
-			if(Game.getObjectById(myRooms[currentRoomIndex].memory.storageOfRoom[0]).store[RESOURCE_POWER] > 0) {
+		if(Game.getObjectById(myRooms[currentRoomIndex].memory.storages[0]) != undefined) {
+			if(Game.getObjectById(myRooms[currentRoomIndex].memory.storages[0]).store[RESOURCE_POWER] > 0) {
 				myRooms[currentRoomIndex].memory.need.push(1);
 			}
 			else {
