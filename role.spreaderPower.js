afca655e6e4d5db8a0ebe2521dfd5b1f46ba832d
Thomas Cibils <thomas.cibils@gmail.com>
2018-04-08 11:03:32 +0200

    console log debug

--- role.spreaderPower.js
+++ role.spreaderPower.js
@@ -27,17 +27,19 @@ var roleSpreaderPower = {
 		
 		if(!creep.memory.gathering) {
 			if(_.sum(creep.carry) > 0) {
+				
 				if(creep.transfer(Game.getObjectById(creep.room.memory.powerSpawningPoints[0]), RESOURCE_POWER) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(Game.getObjectById(creep.room.memory.powerSpawningPoints[0]));
 				}
 				if(Game.getObjectById(creep.room.memory.powerSpawningPoints[0]).energy < 100) {
+					console.log('ere')
 					if(creep.transfer(Game.getObjectById(creep.room.memory.storages[0]), RESOURCE_POWER) == ERR_NOT_IN_RANGE) {
 						creep.moveTo(Game.getObjectById(creep.room.memory.storages[0]));
 					}
 				}
 			}
 			if(_.sum(creep.carry) == 0) {
-				if(creep.ticksToLive >= 130){
+				if(creep.ticksToLive >= 130 && Game.getObjectById(creep.room.memory.powerSpawningPoints[0]).energy == Game.getObjectById(creep.room.memory.powerSpawningPoints[0]).energyCapacity){
 					creep.memory.gathering = true;
 				}
 			}
