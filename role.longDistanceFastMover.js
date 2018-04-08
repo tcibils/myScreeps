e3ec657485ff4c078731d3c82703c8d98a65fb30
Thomas Cibils <thomas.cibils@gmail.com>
2018-04-08 19:34:40 +0200

    adhoc debug

--- role.longDistanceFastMover.js
+++ role.longDistanceFastMover.js
@@ -189,11 +189,13 @@ var longDistanceFastMover = {
             if(creep.room.name != creep.memory.targetRoom) {
                 // Using the need origin position.
 				if(creep.memory.needOriginPos == undefined ) {
-					console.log('creep ' + creep.name + ' in room ' + creep.room.name + ' pos '+ creep.pos + ' has no need origin pos')
+					console.log('ISSUE - creep ' + creep.name + ' in room ' + creep.room.name + ' pos '+ creep.pos + ' has no need origin pos')
 				}
-        		let targetEnergySourcePos = new RoomPosition(creep.memory.needOriginPos.x, creep.memory.needOriginPos.y, creep.memory.needOriginPos.roomName);
-                creep.moveTo(targetEnergySourcePos, {visualizePathStyle: {stroke: '#08ff00'}, reusePath: 10});                
-            }
+				else {
+					let targetEnergySourcePos = new RoomPosition(creep.memory.needOriginPos.x, creep.memory.needOriginPos.y, creep.memory.needOriginPos.roomName);
+					creep.moveTo(targetEnergySourcePos, {visualizePathStyle: {stroke: '#08ff00'}, reusePath: 10});                
+				}
+			}
             
         }
     }
