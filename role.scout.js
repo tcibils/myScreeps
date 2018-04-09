0ac0b2722717030e809fd7be950a9fbd772fd254
Thomas Cibils <thomas.cibils@gmail.com>
2018-04-09 21:47:42 +0200

    not memorizing info about rooms too far away
    Scout will now remember info about rooms not too far away, in terms of source, players, and power. The max limits are set as parameters at the player's hand.

--- role.scout.js
+++ role.scout.js
@@ -15,6 +15,10 @@ var scout = {
         // Current room is the room we currently are.
         // We store this in the memory, so that we can check when we changed room
 
+		let closestMyRoomDistanceToCheckSources = 6;
+		let closestMyRoomDistanceToCheckPlayers = 6;
+		let closestMyRoomDistanceToCheckPower = 8;
+		
         // If undefined, then we set it as the current room
         if(creep.memory.currentRoom == undefined) {
             creep.memory.currentRoom = creep.room.name;
@@ -44,130 +48,142 @@ var scout = {
 		// current room memory will be different from creep room only the first time it gets in a new room
         if(creep.memory.currentRoom != creep.room.name) {
 			
-			// let myRooms = _.filter(Game.rooms, (currentRoom) => currentRoom.controller != undefined && currentRoom.controller.my);
-			// let minimalDistance = 1000;
-			// for loop on my rooms to find the closest one with getRoomLinearDistance
-			// if minimal distance is lower than 4, then do the following.
-			
-            // Storing sources of the room : ID, position and maxEnergy - only if undefined, as it won't change in time...
-            if(creep.room.memory.sources == undefined || creep.room.memory.sourcesPos == undefined || creep.room.memory.sourcesMax == undefined) {
-                creep.room.memory.sources = [];
-                creep.room.memory.sourcesPos = [];
-                creep.room.memory.sourcesMax = [];
-
-                var sourcesOfRoom = creep.room.find(FIND_SOURCES);
-                if(sourcesOfRoom.length > 0) {
-                    for(let currentSourceIndex= 0; currentSourceIndex<sourcesOfRoom.length; currentSourceIndex++) {
-                        creep.room.memory.sources.push(sourcesOfRoom[currentSourceIndex].id);
-                        creep.room.memory.sourcesMax.push(sourcesOfRoom[currentSourceIndex].energyCapacity);
-                        creep.room.memory.sourcesPos.push(sourcesOfRoom[currentSourceIndex].pos);
-                    }
-                }
-            }
-			
-			if(creep.room.memory.controller == undefined || creep.room.memory.controllerPos == undefined) {
-				if(creep.room.controller) {
-					creep.room.memory.controller = creep.room.controller.id;
-					creep.room.memory.controllerPos = creep.room.controller.pos;
-				}
-				else {
-					creep.room.memory.controller = 'undefined';
-					creep.room.memory.controllerPos = 'undefined';
+			// We find the closest of room "globally", from bird's eye - "game.map.getRoomLinearDistance" is not expensive at all
+			let myRooms = _.filter(Game.rooms, (currentRoom) => currentRoom.controller != undefined && currentRoom.controller.my);
+			let closestMyRoomDistance = 1000;
+			for(let currentRoomIndex = 0; currentRoomIndex < myRooms.length; currentRoomIndex++) {
+				let distanceOfAssessedRoom = Game.map.getRoomLinearDistance(creep.room.name, myRooms[currentRoomIndex].name);
+				if(distanceOfAssessedRoom < closestMyRoomDistance) {
+					closestMyRoomDistance = distanceOfAssessedRoom;
 				}
 			}
-
-            // Storing the owner of the room, if undefined or out of date
+			// Now we know that the closest of my rooms is at closestMyRoomDistance.
 			
-			// If there is a controller
-			if(creep.room.controller != undefined) {
-				// If the variable is already stored in memory
-				if(creep.room.memory.roomOwner != undefined) {
-					// If the owner is not the same anymore
-					if(creep.room.memory.roomOwner != creep.room.controller.owner) {
-						// We change the data in memory
-						creep.room.memory.roomOwner = creep.room.controller.owner;
-					}
-				}
-				// If the variable is not yet defined
-				else {
-					// We define it
-					creep.room.memory.roomOwner = creep.room.controller.owner;
-				}
-				
-				// Same for reservation
-				// If the variable is defined
-				if(creep.room.memory.roomOwnerReservation != undefined) {
-					if(creep.room.controller.reservation != undefined) {
-						// If it's not the same player having reserved it anymore
-						if(creep.room.memory.roomOwnerReservation != creep.room.controller.reservation.username) {
-							// We update the info
-							creep.room.memory.roomOwnerReservation = creep.room.controller.reservation.username;
-						}					
-					}
-					else {
-						creep.room.memory.roomOwnerReservation = undefined;
-					}
-				}
-				// If the variable is not yet defined
-				else {
-					// We initialize it
-					if(creep.room.controller.reservation != undefined) {
-						creep.room.memory.roomOwnerReservation = creep.room.controller.reservation.username;
-					}
-				}
-				
-			}
-			// If there is no controller, we keep all at undefined
-			else {
-				creep.room.memory.roomOwner = undefined;
-				creep.room.memory.roomOwnerReservation = undefined;
-				// TRICKY THAT ! If there is no controller, then we don't have an interest in LDHarvesting the room.
-				// This rules out the corridor rooms, which have neither controller nor source
-				// It also rules out central rools, which have multiple powerful sources, but no controller, and as sources are defended we currently don't know how to manage it
-				// So it conviniently rules out certain rooms with a biaised but useful and simple indicator
-				creep.room.memory.noEnergyInterest = true;
-			}
+			// If my closest room is farther than the parameter setup rooms away, no way we'll harvest it {
+			if(closestMyRoomDistance < closestMyRoomDistanceToCheckSources) {
+				// Storing sources of the room : ID, position and maxEnergy - only if undefined, as it won't change in time...
+				if(creep.room.memory.sources == undefined || creep.room.memory.sourcesPos == undefined || creep.room.memory.sourcesMax == undefined) {
+					creep.room.memory.sources = [];
+					creep.room.memory.sourcesPos = [];
+					creep.room.memory.sourcesMax = [];
 
-            // No "if" on power memory as we want to update it anyway
-
-            creep.room.memory.powerSources = [];
-            creep.room.memory.powerSourcesPos = [];
-            creep.room.memory.powerSourcesMax = [];
-            creep.room.memory.powerSourcesHits = [];
-            creep.room.memory.powerSourcesHitsMax = [];
-            creep.room.memory.powerSourcesTime = []; // time to live of power source
-            creep.room.memory.powerSourcesDiscoveryTime = [];
-            creep.room.memory.powerSourceFreeSpots = [];
-			
-			// We find the power sources
-            var powerSourcesOfRoom = creep.room.find(FIND_STRUCTURES, {filter: function(object) {return object.structureType == STRUCTURE_POWER_BANK}});
-			
-			// If there is some
-            if(powerSourcesOfRoom.length > 0) {
-				// We store all their information in the memory
-                for(let currentPowerSourceIndex = 0; currentPowerSourceIndex < powerSourcesOfRoom.length; currentPowerSourceIndex++) {
-                    creep.room.memory.powerSources.push(powerSourcesOfRoom[currentPowerSourceIndex].id);
-                    creep.room.memory.powerSourcesPos.push(powerSourcesOfRoom[currentPowerSourceIndex].pos);
-                    creep.room.memory.powerSourcesMax.push(powerSourcesOfRoom[currentPowerSourceIndex].power);
-                    creep.room.memory.powerSourcesHits.push(powerSourcesOfRoom[currentPowerSourceIndex].hits);
-                    creep.room.memory.powerSourcesHitsMax.push(powerSourcesOfRoom[currentPowerSourceIndex].hitsMax);
-                    creep.room.memory.powerSourcesTime.push(powerSourcesOfRoom[currentPowerSourceIndex].ticksToDecay);
-                    creep.room.memory.powerSourcesDiscoveryTime.push(Game.time);
-					
-					// Here we look around the power bank to see how much free spots there are.
-					// 1 is too less for us to harvest it.
-					var counter = 0;
-					for(var a = -1; a<=1; a++) {
-						for(var b = -1; b<=1; b++) {
-							let terrainToBeAssessed = Game.map.getTerrainAt(powerSourcesOfRoom[currentPowerSourceIndex].pos.x + a, powerSourcesOfRoom[currentPowerSourceIndex].pos.y + b, creep.room.name);
-							if (terrainToBeAssessed == 'plain' || terrainToBeAssessed == 'swamp') {
-								counter ++;
-							}
+					var sourcesOfRoom = creep.room.find(FIND_SOURCES);
+					if(sourcesOfRoom.length > 0) {
+						for(let currentSourceIndex= 0; currentSourceIndex<sourcesOfRoom.length; currentSourceIndex++) {
+							creep.room.memory.sources.push(sourcesOfRoom[currentSourceIndex].id);
+							creep.room.memory.sourcesMax.push(sourcesOfRoom[currentSourceIndex].energyCapacity);
+							creep.room.memory.sourcesPos.push(sourcesOfRoom[currentSourceIndex].pos);
 						}
 					}
-                    creep.room.memory.powerSourceFreeSpots.push(counter);
-                }
-            }
+				}
+				
+				if(creep.room.memory.controller == undefined || creep.room.memory.controllerPos == undefined) {
+					if(creep.room.controller) {
+						creep.room.memory.controller = creep.room.controller.id;
+						creep.room.memory.controllerPos = creep.room.controller.pos;
+					}
+					else {
+						creep.room.memory.controller = 'undefined';
+						creep.room.memory.controllerPos = 'undefined';
+					}
+				}
+			}
+            // Storing the owner of the room, if undefined or out of date
+			
+			if(closestMyRoomDistance < closestMyRoomDistanceToCheckPlayers) {
+				// If there is a controller
+				if(creep.room.controller != undefined) {
+					// If the variable is already stored in memory
+					if(creep.room.memory.roomOwner != undefined) {
+						// If the owner is not the same anymore
+						if(creep.room.memory.roomOwner != creep.room.controller.owner) {
+							// We change the data in memory
+							creep.room.memory.roomOwner = creep.room.controller.owner;
+						}
+					}
+					// If the variable is not yet defined
+					else {
+						// We define it
+						creep.room.memory.roomOwner = creep.room.controller.owner;
+					}
+					
+					// Same for reservation
+					// If the variable is defined
+					if(creep.room.memory.roomOwnerReservation != undefined) {
+						if(creep.room.controller.reservation != undefined) {
+							// If it's not the same player having reserved it anymore
+							if(creep.room.memory.roomOwnerReservation != creep.room.controller.reservation.username) {
+								// We update the info
+								creep.room.memory.roomOwnerReservation = creep.room.controller.reservation.username;
+							}					
+						}
+						else {
+							creep.room.memory.roomOwnerReservation = undefined;
+						}
+					}
+					// If the variable is not yet defined
+					else {
+						// We initialize it
+						if(creep.room.controller.reservation != undefined) {
+							creep.room.memory.roomOwnerReservation = creep.room.controller.reservation.username;
+						}
+					}
+					
+				}
+				// If there is no controller, we keep all at undefined
+				else {
+					creep.room.memory.roomOwner = undefined;
+					creep.room.memory.roomOwnerReservation = undefined;
+					// TRICKY THAT ! If there is no controller, then we don't have an interest in LDHarvesting the room.
+					// This rules out the corridor rooms, which have neither controller nor source
+					// It also rules out central rools, which have multiple powerful sources, but no controller, and as sources are defended we currently don't know how to manage it
+					// So it conviniently rules out certain rooms with a biaised but useful and simple indicator
+					creep.room.memory.noEnergyInterest = true;
+				}
+			}
+
+			if(closestMyRoomDistance < closestMyRoomDistanceToCheckPower) {
+				// No "if" on power memory as we want to update it anyway
+
+				creep.room.memory.powerSources = [];
+				creep.room.memory.powerSourcesPos = [];
+				creep.room.memory.powerSourcesMax = [];
+				creep.room.memory.powerSourcesHits = [];
+				creep.room.memory.powerSourcesHitsMax = [];
+				creep.room.memory.powerSourcesTime = []; // time to live of power source
+				creep.room.memory.powerSourcesDiscoveryTime = [];
+				creep.room.memory.powerSourceFreeSpots = [];
+				
+				// We find the power sources
+				var powerSourcesOfRoom = creep.room.find(FIND_STRUCTURES, {filter: function(object) {return object.structureType == STRUCTURE_POWER_BANK}});
+				
+				// If there is some
+				if(powerSourcesOfRoom.length > 0) {
+					// We store all their information in the memory
+					for(let currentPowerSourceIndex = 0; currentPowerSourceIndex < powerSourcesOfRoom.length; currentPowerSourceIndex++) {
+						creep.room.memory.powerSources.push(powerSourcesOfRoom[currentPowerSourceIndex].id);
+						creep.room.memory.powerSourcesPos.push(powerSourcesOfRoom[currentPowerSourceIndex].pos);
+						creep.room.memory.powerSourcesMax.push(powerSourcesOfRoom[currentPowerSourceIndex].power);
+						creep.room.memory.powerSourcesHits.push(powerSourcesOfRoom[currentPowerSourceIndex].hits);
+						creep.room.memory.powerSourcesHitsMax.push(powerSourcesOfRoom[currentPowerSourceIndex].hitsMax);
+						creep.room.memory.powerSourcesTime.push(powerSourcesOfRoom[currentPowerSourceIndex].ticksToDecay);
+						creep.room.memory.powerSourcesDiscoveryTime.push(Game.time);
+						
+						// Here we look around the power bank to see how much free spots there are.
+						// 1 is too less for us to harvest it.
+						var counter = 0;
+						for(var a = -1; a<=1; a++) {
+							for(var b = -1; b<=1; b++) {
+								let terrainToBeAssessed = Game.map.getTerrainAt(powerSourcesOfRoom[currentPowerSourceIndex].pos.x + a, powerSourcesOfRoom[currentPowerSourceIndex].pos.y + b, creep.room.name);
+								if (terrainToBeAssessed == 'plain' || terrainToBeAssessed == 'swamp') {
+									counter ++;
+								}
+							}
+						}
+						creep.room.memory.powerSourceFreeSpots.push(counter);
+					}
+				}
+			}
 
         }
 
