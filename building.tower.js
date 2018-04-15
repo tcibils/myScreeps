be6613b0857100601180536dfc38f8f1c1dd7c4b
Thomas Cibils <thomas.cibils@gmail.com>
2018-04-15 09:21:46 +0200

    No changes
    Trying to write code but will rather commit on another branch

--- building.tower.js
+++ building.tower.js
@@ -3,7 +3,7 @@ var towerRole = {
 
     /** @param {Creep} creep **/
     run: function(tower) {
-            var targetAttack = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS/*, {filter: function(object) {return object.getActiveBodyparts(ATTACK)>0}}*/);
+		var targetAttack = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS/*, {filter: function(object) {return object.getActiveBodyparts(ATTACK)>0}}*/);
                 
         /*
         var targetsRepair = tower.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.hits < s.hitsMax && s.hits < 25000});
@@ -14,7 +14,7 @@ var towerRole = {
         if(targetAttack != null) {
             tower.attack(targetAttack);
         }
-        
+		 
     }   
 };
 
